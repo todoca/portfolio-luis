fd42a2bf3242318241a75fbd72fbdeb87b52a094
Grupo Todoca, C.A <grupotodoca@gmail.com>
2021-06-02 15:22:27 -0700

    About ready

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,44 +1,61 @@
 import "./about.scss";
-import ProgressBar from "../progressBar/ProgressBar";
+import { Player, Controls } from "@lottiefiles/react-lottie-player";
 
 export default function About() {
   return (
     <div className="about" id="about">
       <div className="left">
         <h2>About Me</h2>
-        <h1>MY MISSION IS DESIGN & DEVELOP THE BEST WEBSITES AROUND</h1>
+        <div className="lottieabout">
+          <Player autoplay loop src="assets/dron.json" />
+        </div>
+        <h1>
+          My goal is design & develop awesome cross-platform apps for the world
+          with love.
+        </h1>
         <h3>
-          I mastered technology to make the best possible software that’ll
-          hopefully result in making people’s lives a bit better, one line of
-          code at a time.
+          Mi nombre es Luis Alejandro Marcano, venezolano, ingeniero
+          electrónico, freelance, empresario, amante de la ciencia y tecnología.
         </h3>
       </div>
       <div className="right">
-        <div className="container">
-          <div className="item">
-            <h4>
-              Website & Web Applications <span>80%</span>
-            </h4>
-            <ProgressBar width={100} percent={0.3} />
+        <div className="top">
+          <div className="lottieabout">
+            <Player autoplay loop src="assets/history.json" />
           </div>
-          <div className="item">
-            <h4>
-            Mobile Apps <span>80%</span>
-            </h4>
-            <ProgressBar width={100} percent={0.3} />
-          </div>
-          <div className="item">
-            <h4>
-            Software Design & Architecture <span>80%</span>
-            </h4>
-            <ProgressBar width={100} percent={0.3} />
-          </div>
-          <div className="item">
-            <h4>
-              Web design <span>80%</span>
-            </h4>
-            <ProgressBar width={100} percent={0.3} />
-          </div>
+          <h1>My History</h1>
+        </div>
+        <div className="center">
+          <p>
+            Mi primer encuentro fue con un Sistema Operativo MS-DOS y un juego
+            llamado California Games, a partir de allí, entendí las infinitas
+            posibilidades de crear algo maravilloso.
+          </p><br/>
+          <p>
+            En la Universidad, interactuamos con lenguajes como C++ y
+            Ensamblador para los Microcontroladores, permitiendo un sublime
+            encuentro con las infinitas líneas de código que se ocultan
+            sutilmente detras de lo tangible, cumpliendo funciones y tareas
+            imperceptibles para la mayoría de las personas, fue como magia.
+          </p><br/>
+          <p>
+            Luego de graduarme de ingeniero en el 2006, empecé trabajando en
+            grandes empresas del sector de Telecomunicaciones y desarrollo de
+            soluciones IT durante 6 años, En el 2012 decidí crear mi propia
+            empresa de desarrollo de software, Todoca, con la intención de
+            brindar Servicios Tecnológicos y Soluciones Integrales a personas
+            que se encuentran en constante innovación y evolución. Finalmente,
+            en 2019, decidimos dedicarme completamente a al area programación,
+            diseño y desarrollo, con el propósito de crear aplicaciones
+            multiplataformas que mejoren la productividad y sirvamos de ayuda a
+            otros con la tecnología.
+          </p><br/>
+          <p>
+            Actualmente dirijo Todoca, trabajamos remotamente a diferentes
+            lugares del mundo. Si estás buscando eficiencia, profesionalidad y
+            dedicación, ¡cuenta conmigo!
+          </p>
+          <div className="bottom"></div>
         </div>
       </div>
     </div>
--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -1,9 +1,23 @@
-import './skills.scss'
+import "./skills.scss";
+import ProgressBar from "../progressBar/ProgressBar";
 
 export default function Skills() {
-    return (
-        <div className="skills" id="skills">
-           <h1>Skills</h1> 
+  return (
+    <div className="skills" id="skills">
+      <div className="sliderlogos">Slider de Logos</div>
+      <div className="left">
+        <div className="top">
+          <h1>My Skills</h1>
         </div>
-    )
+        <div className="bottom">
+          <div className="frontend">
+            <h1>Front End</h1>
+          </div>
+          <div className="backend">
+            <h1>Back End</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
